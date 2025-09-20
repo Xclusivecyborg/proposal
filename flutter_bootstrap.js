c99a56f4d483b43df95234720e829f7ed5274add
Xclusivecyborg <xclusivecyborg@gmail.com>
2025-09-20 02:03:08 +0100

    updates to  proposal commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1308354349"
+    serviceWorkerVersion: "2599879445"
   }
 });

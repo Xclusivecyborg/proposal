7be0fab0cf0d006635e672848423aa8e6f0ea774
Xclusivecyborg <xclusivecyborg@gmail.com>
2025-09-20 02:48:09 +0100

    remove rive asset

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "920547860"
+    serviceWorkerVersion: "1918292827"
   }
 });

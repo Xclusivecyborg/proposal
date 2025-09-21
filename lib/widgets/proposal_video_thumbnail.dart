0317de186b06617430065432344034b1c3612615
Xclusivecyborg <xclusivecyborg@gmail.com>
2025-09-21 20:32:29 +0100

    feat: Update video player UI to enhance user interaction and improve image scaling in StaggeredGridView

--- lib/widgets/proposal_video_thumbnail.dart
+++ lib/widgets/proposal_video_thumbnail.dart
@@ -30,36 +30,53 @@ class _PlayerState extends State<Player> {
   Widget build(BuildContext context) {
     return _controller.value.isInitialized
         ? Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Tap to Play',
-                  style:
-                      context.textTheme.t14W500.copyWith(color: Colors.white70),
-                ),
-                IconButton(
-                  iconSize: 50,
-                  color: Colors.white,
-                  icon: Icon(
-                    _controller.value.isPlaying
-                        ? Icons.pause
-                        : Icons.play_arrow,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Transform.scale(
+                    scale: 1.5,
+                    child: VideoPlayer(
+                      _controller,
+                    ),
                   ),
-                  onPressed: () {
-                    showGeneralDialog(
-                      context: context,
-                      pageBuilder: (context, _, __) {
-                        return NetworkVideoPlayer(
-                          networkImage: widget.videoUrl,
-                          shouldPlay: true,
-                        );
-                      },
-                    );
-                  },
-                ),
-              ],
+                  Positioned.fill(
+                    child: Container(color: Colors.black54),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Tap to Play',
+                        style: context.textTheme.t14W500
+                            .copyWith(color: Colors.white70),
+                      ),
+                      IconButton(
+                        iconSize: 50,
+                        color: Colors.white,
+                        icon: Icon(
+                          _controller.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                        ),
+                        onPressed: () {
+                          showGeneralDialog(
+                            context: context,
+                            pageBuilder: (context, _, __) {
+                              return NetworkVideoPlayer(
+                                networkImage: widget.videoUrl,
+                                shouldPlay: true,
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           )
         : SizedBox();
--- lib/widgets/staggered_grid_view.dart
+++ lib/widgets/staggered_grid_view.dart
@@ -69,18 +69,18 @@ class _StaggeredGridViewState extends State<StaggeredGridView>
                           borderRadius: BorderRadius.circular(15),
                           child: CachedNetworkImage(
                             width:
-                                (100 * MediaQuery.of(context).devicePixelRatio)
+                                (50 * MediaQuery.of(context).devicePixelRatio)
                                     .round()
                                     .w,
                             height:
-                                (75 * MediaQuery.of(context).devicePixelRatio)
+                                (35 * MediaQuery.of(context).devicePixelRatio)
                                     .round()
                                     .h,
                             memCacheWidth:
-                                (100 * MediaQuery.of(context).devicePixelRatio)
+                                (50 * MediaQuery.of(context).devicePixelRatio)
                                     .round(),
                             memCacheHeight:
-                                (75 * MediaQuery.of(context).devicePixelRatio)
+                                (35 * MediaQuery.of(context).devicePixelRatio)
                                     .round(),
                             imageUrl: imageUrl,
                             fit: BoxFit.cover,
